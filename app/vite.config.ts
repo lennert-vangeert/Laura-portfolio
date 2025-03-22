19fe02bf36c3cefd2588e929d42d2ab27c7f5e97
Lennert Van Geert <lennert.vangeert@gmail.com>
2025-03-22 14:51:24 +0100

    .

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import * as path from "path";
+import { fileURLToPath } from "url";
 import svgr from "vite-plugin-svgr";
 
+// Using import.meta.url to construct __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
